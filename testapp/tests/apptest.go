cb6751f0b75ae3f8143b32f68f0985c46c67aa76
Jan Olszak <janekolszak@gmail.com>
2015-07-07 21:59:29 +0200

    Test app works with Revel 0.12.0

--- testapp/tests/apptest.go
+++ testapp/tests/apptest.go
@@ -1,39 +1,39 @@
 package tests
 
 import (
-	"github.com/jgraham909/revmgo"
-	"github.com/jgraham909/revmgo/testapp/app/models"
-	"github.com/revel/revel"
+    "github.com/jgraham909/revmgo"
+    "github.com/jgraham909/revmgo/testapp/app/models"
+    "github.com/revel/revel/testing"
 )
 
 type AppTest struct {
-	revel.TestSuite
+    testing.TestSuite
 }
 
 func (t *AppTest) Before() {
-	// Make sure our collection is clean
-	models.Collection(revmgo.Session).DropCollection()
+    // Make sure our collection is clean
+    models.Collection(revmgo.Session).DropCollection()
 }
 
-func (t AppTest) TestThatIndexPageWorks() {
-	t.Get("/")
-	t.AssertOk()
-	t.AssertContentType("text/html; charset=utf-8")
+func (t *AppTest) TestThatIndexPageWorks() {
+    t.Get("/")
+    t.AssertOk()
+    t.AssertContentType("text/html; charset=utf-8")
 
 }
 
-func (t AppTest) TestSave() {
-	b := models.GetBook("MobyDick")
-	t.AssertEqual("Moby Dick", b.Title)
-	b.Save(revmgo.Session)
-	d := models.GetBookByObjectId(revmgo.Session, b.Id)
-	t.AssertEqual(b.Title, d.Title)
-	t.AssertEqual(b.Id, d.Id)
-	t.AssertEqual(b.Body, d.Body)
-	t.AssertEqual(b.Tags, d.Tags)
+func (t *AppTest) TestSave() {
+    b := models.GetBook("MobyDick")
+    t.AssertEqual("Moby Dick", b.Title)
+    b.Save(revmgo.Session)
+    d := models.GetBookByObjectId(revmgo.Session, b.Id)
+    t.AssertEqual(b.Title, d.Title)
+    t.AssertEqual(b.Id, d.Id)
+    t.AssertEqual(b.Body, d.Body)
+    t.AssertEqual(b.Tags, d.Tags)
 }
 
 func (t *AppTest) After() {
-	// Cleanup any mess we made
-	models.Collection(revmgo.Session).DropCollection()
+    // Cleanup any mess we made
+    models.Collection(revmgo.Session).DropCollection()
 }
